21aac31ffef7b60b0b0723ee27e9c5f4719d4bf9
Vanilla X <mail@liyaodong.com>
2016-07-26 18:40:12 +0800

    Add upload strategy

--- app/scripts.babel/index.js
+++ app/scripts.babel/index.js
@@ -1,3 +1,30 @@
 'use strict';
 
-alert('index');
+Dropzone.autoDiscover = false;
+
+const $ = x => document.querySelector(x);
+
+const $uploader = $('#uploader');
+
+chrome.storage.sync.get({
+  token: null,
+}, d => {
+  if (d.token === null) {
+    alert('请先配置 token');
+    return;
+  }
+
+  $('#uploader input[name=token]').value = d.token;
+});
+
+const dropzone = new Dropzone('#uploader', {
+  url: 'http://upload.qiniu.com',
+  method: 'POST',
+  uploadMultiple: false,
+  success: (...args) => {
+    console.log(args);
+  },
+  error : e => {
+    alert(e.xhr.responseText);
+  }
+});
